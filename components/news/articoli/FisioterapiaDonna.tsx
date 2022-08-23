5d3cf4e8ae83d10b415542f1fdbdffbe773e6635
digitalthree <web@digital-three.com>
2022-08-23 17:18:04 +0200

    Implementate modifiche richieste dal cliente v11

--- components/news/articoli/FisioterapiaDonna.tsx
+++ components/news/articoli/FisioterapiaDonna.tsx
@@ -27,17 +27,17 @@ export const FisioterapiaDonna: React.FC<FisioterapiaDonnaProps> = ({}) => {
             <ul className="list-disc px-10">
                 <li>Riabilitazione dermatofunzionale e Fisioestetica che vanno a trattare tutte le patologie come
                     cellulite, adipe localizzato, smagliature, dimagrimento localizzato, segni del tempo sul viso, gambe
-                    gonfie e pesanti. Ci occupiamo di Fisioestetica dal 2015 e siamo specializzati nellì'elaborazione di
+                    gonfie e pesanti. Ci occupiamo di Fisioestetica dal 2015 e siamo specializzati nell’elaborazione di
                     programmi riabilitativi personalizzati che permettono di ottenere risultati duraturi nel tempo.
                 </li>
             </ul>
             <p className="text-[18px] mb-4 mt-4">
                 Utilizziamo le migliori tecnologie e apparecchiature con certificazioni medicali come: Tecar, Onde
-                d'urto, Criolipolisi, Physio TT, Campi elettromagnetici ad alta intensità, Cavitazione, Radiofrequenza.
+                d’urto, Criolipolisi, Physio TT, Campi elettromagnetici ad alta intensità, Cavitazione, Radiofrequenza.
             </p>
             <ul className="list-disc px-10">
-                <li>Riabilitazione posturale ed esercizio terapeutico. Siamo l'unico centro a Teramo specializzati nella
-                    cura e prevenzione di patologie della colonna vertebrale e apparato viscerale mediante l'utilizzo di
+                <li>Riabilitazione posturale ed esercizio terapeutico. Siamo l’unico centro a Teramo specializzati nella
+                    cura e prevenzione di patologie della colonna vertebrale e apparato viscerale mediante l’utilizzo di
                     tecniche di terapia manuale, posturale metodo Mezieres, ginnastica Ipopressiva, Pilates Fisio.
                 </li>
             </ul>
